7a635c6e62cb2e7b9769fb075e1f9cc9dd848b0f
Veronica <vero.fullstack@gmail.com>
2020-11-04 12:24:17 +0100

    Button Component casi listo

--- src/modules/Content/checkSortPanel/CheckSortPanel.js
+++ src/modules/Content/checkSortPanel/CheckSortPanel.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { TopPanel } from './checksortpanel.style';
+import Button from './../../../components/Button/Button';
 
 const CheckSortPanel = props => {
+
+    const showList = useCallback(
+        () => {
+            console.log('Mostrar la lista');
+        },[], // Le dice a React que memorice independientemente de los argumentos.
+    );
+
+    const sortMenu = useCallback(
+        () => {
+            console.log('Ordenar el menu');
+        },[],
+    );
+
     return(
         <TopPanel className="sortcheckPanel">
-            <div className="button">Check List</div>
-            <div className="button">Sort</div>
+            <Button
+            disabled
+                text="Check list"
+                onClick={showList}
+            />
+            <Button
+                text="Sort"
+                onClick={sortMenu}
+            />
         </TopPanel>
     );
 }
--- src/modules/Content/checkSortPanel/checksortpanel.style.js
+++ src/modules/Content/checkSortPanel/checksortpanel.style.js
@@ -1,13 +1,9 @@
 import styled from 'styled-components';
 
 const TopPanel = styled.div`
-  /*Css temporal*/
-  border: 1px solid white;
-  /*Css temporal*/
   padding: ${props => props.theme.spaces.medium};
   display: flex;
   justify-content: space-between;
-
   .button {
     display: flex;
     width: auto;
