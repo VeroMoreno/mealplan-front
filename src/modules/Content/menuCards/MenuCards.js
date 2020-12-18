a10dcb47a6aa154733e978e3bec4138b9dca5778
Veronica <vero.fullstack@gmail.com>
2020-12-18 15:35:13 +0100

    fix reorder function

--- src/modules/Content/menuCards/MenuCards.js
+++ src/modules/Content/menuCards/MenuCards.js
@@ -23,57 +23,19 @@ const MenuCards = () => {
 
   const week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
 
-  const reorder = (list, startIndex, endIndex, startColumn, endColumn) => {
+  const reorder = (list, posStart, posEnd) => {
     const result = Array.from(list.slice(0, 5));
-    // encontramos la columna de inicio dentro de la lista
-    // const startColumnIndex = result.findIndex((item) => item.id === startColumn);
-    // encontramos la columna final dentro de la lista
-    // const endColumnIndex = result.findIndex((item) => item.id === endColumn);
-
-    console.log(startIndex);
-    console.log(endIndex);
-    const firstPart = result.slice(0, startIndex + 1);
-    const secondPart = result.slice(startIndex + 1);
-    const element = result[startIndex];
-    console.log(firstPart);
-    console.log(secondPart);
-    console.log(element);
-    debugger;
-    const [removedS] = result.splice(startIndex, 1);
-    // INTENTOS FRUSTRADOS PARA QUE FUNCIONE, MAL MAL MAL MAAAAAAAAL!
-    let removedE = 0;
-    if (result.length === endIndex) {
-      [removedE] = result.splice(endIndex - 1, 1);
-    } else {
-      [removedE] = result.splice(endIndex, 1);
-    }
-    result.splice(endIndex, 0, removedS);
-    result.splice(startIndex, 0, removedE);
+    const valEnd = result[posEnd];
+    result[posEnd] = result[posStart];
+    result[posStart] = valEnd;
     return result;
-  };
-
-  const test = (list, posStart, posEnd) => {
-    /* let lista = ['gato', 'perro', 'oveja', 'cabra'];
-    function cambiar(array, posStart, posEnd) {
-      let valEnd = array[posEnd];
-      array[posEnd] = array[posStart];
-      array[posStart] = valEnd;
-      return array;
-    }
-    console.info(cambiar(lista, 0, 2));
-    console.info(cambiar(lista, 1, 2)); */
-    const valEnd = list[posEnd];
-    list[posEnd] = list[posStart];
-    list[posStart] = valEnd;
-    return list;
   };
 
   const onDragEndOne = (result) => {
     const { destination, source } = result;
     if (!result.destination) return;
     if (result.destination.index === result.source.index) return;
-    // eslint-disable-next-line max-len
-    const newElements = reorder(lunchData, source.index, destination.index, source.droppableId, destination.droppableId);
+    const newElements = reorder(lunchData, source.index, destination.index);
     setLunchData(newElements);
   };
 
@@ -110,14 +72,14 @@ const MenuCards = () => {
           <DragDropContext onDragEnd={onDragEndOne}/* where happens */>
             <ul className="calendar__week lunch">
               {lunchData.slice(0, 5).map((el, i) => (
-                <Droppable droppableId={el.id} key={el.id}/* drop items: 2 COLUMNAS */>
+                <Droppable droppableId={el.id} key={el.id}/* drop items: two columns */>
                   {(provided) => (
                     <li
                       ref={provided.innerRef}
                       className="calendar__day day"
                       {...provided.droppableProps}
                     >
-                      <Draggable draggableId={el.id} index={i} key={el.id}/* PUEDOMOVER(CARDS) */>
+                      <Draggable draggableId={el.id} index={i} key={el.id}/* canmove(CARDS) */>
                         {(provided) => (
                           // eslint-disable-next-line max-len
                           <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
