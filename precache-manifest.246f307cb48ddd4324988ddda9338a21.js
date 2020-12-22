1ce2ae14d348469da3ac6d309e3ba55f07eb57b8
Carlos <carlos.10.05.1993@gmail.com>
2020-12-22 12:33:16 +0100

    Updates

---
+++ precache-manifest.246f307cb48ddd4324988ddda9338a21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5974bbbdd133137cf749d816eb2e8a",
+    "url": "/meal-plan/index.html"
+  },
+  {
+    "revision": "315b9a742f9c7041c7a0",
+    "url": "/meal-plan/static/css/main.4630cddb.chunk.css"
+  },
+  {
+    "revision": "0e61436ea8bcd5b5de2e",
+    "url": "/meal-plan/static/js/2.a484ed74.chunk.js"
+  },
+  {
+    "revision": "b952d6d32f30bab0684e61579c5d99b7",
+    "url": "/meal-plan/static/js/2.a484ed74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315b9a742f9c7041c7a0",
+    "url": "/meal-plan/static/js/main.2b7a84ff.chunk.js"
+  },
+  {
+    "revision": "efc07ce855fe7904a074",
+    "url": "/meal-plan/static/js/runtime-main.62b2ed27.js"
+  }
+]);
