40b86583af5e60e5d75de151003dc070f39948f8
Veronica <veronica.moreno.work@gmail.com>
2021-05-04 21:20:49 +0200

    Updates

---
+++ precache-manifest.3789bc8d20e1e0f47c975dc807ecc0d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6eca9fa58adc0da8a71c3d45bd76493",
+    "url": "/mealplan-front/index.html"
+  },
+  {
+    "revision": "76c4d9c01b743a39c574",
+    "url": "/mealplan-front/static/css/main.123fd5e9.chunk.css"
+  },
+  {
+    "revision": "ec3ce597ae80dc78e24a",
+    "url": "/mealplan-front/static/js/2.146ad3cb.chunk.js"
+  },
+  {
+    "revision": "b952d6d32f30bab0684e61579c5d99b7",
+    "url": "/mealplan-front/static/js/2.146ad3cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c4d9c01b743a39c574",
+    "url": "/mealplan-front/static/js/main.2fb51981.chunk.js"
+  },
+  {
+    "revision": "fc04e4627f75a8a1fa6c",
+    "url": "/mealplan-front/static/js/runtime-main.08cb9ece.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8c854577770fc1c043696ce6aced729b.js"
+  "/mealplan-front/precache-manifest.3789bc8d20e1e0f47c975dc807ecc0d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mealplan-front/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
